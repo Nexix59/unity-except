488601e43d2d619e4df097865a1b1bed61cfeaab
Nexix <pandadank5@gmail.com>
2022-10-26 20:20:31 -0700

    gggggg
    frfrfrfrgrgrgrg

--- Golden Top/Assets/move.cs
+++ Golden Top/Assets/move.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed;
+
+    public Rigidbody2D rb;
+
+    private Vector2 moveDirection;
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessInputs();
+    }
+
+    void FixedUpdate()
+    {
+        //physic calculations 
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized; // BRB
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
     }
 }
